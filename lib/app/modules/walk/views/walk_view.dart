157fcba2346177da458c52b6555a59544a4f7333
dc143cDev <devc143c@gmail.com>
2023-01-16 21:18:35 +0900

    이미지 픽커 직접 제작. get storage 적용

--- lib/app/modules/walk/views/walk_view.dart
+++ lib/app/modules/walk/views/walk_view.dart
@@ -19,7 +19,7 @@ class WalkView extends GetView<WalkController> {
       backgroundColor: bgColor,
       appBar: AppBar(
         title: Text(
-          'Cash Walk',
+          'Walk',
           style: TextStyle(
               fontFamily: 'LS',
               fontSize: 30,
@@ -81,60 +81,6 @@ class WalkView extends GetView<WalkController> {
                       child: PageView(
                         children: [
                           //Indicator type A
-                          TweenAnimationBuilder(
-                              tween: Tween(begin: 0.0, end: 1.0),
-                              duration: Duration(seconds: 8),
-                              builder: (context, value, child) {
-                                int typeAData = (value * 100).ceil();
-                                return Container(
-                                  height: 300,
-                                  width: 500,
-                                  child: Stack(
-                                    children: [
-                                      ShaderMask(
-                                        shaderCallback: (rect) {
-                                          return SweepGradient(
-                                            startAngle: 0.0,
-                                            endAngle: 3.14 * 2,
-                                            stops: [value, value],
-                                            center: Alignment.center,
-                                            colors: [
-                                              Colors.greenAccent,
-                                              Colors.grey.withAlpha(200),
-                                            ],
-                                          ).createShader(rect);
-                                        },
-                                        child: Container(
-                                          margin: EdgeInsets.all(30),
-                                          height: 300,
-                                          width: 500,
-                                          decoration: BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            //image로 바꿔서 ui 변화 가능.
-                                            image: DecorationImage(
-                                                image: Image.asset(
-                                                        'assets/images/radial_scale.png')
-                                                    .image),
-                                          ),
-                                        ),
-                                      ),
-                                      Center(
-                                        child: Container(
-                                          height: 250,
-                                          width: 460,
-                                          decoration: BoxDecoration(
-                                            color: Colors.white,
-                                            shape: BoxShape.circle,
-                                          ),
-                                          child: Center(
-                                            child: Text("$typeAData"),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              }),
                           IndicatorCircularView(),
                           IndicatorCircularView(),
                         ],
