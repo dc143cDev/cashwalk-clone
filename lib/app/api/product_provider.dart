cd4bc0e126594cf141a4ff6058ba1b5428bc67a5
dc143cDev <devc143c@gmail.com>
2023-01-28 23:53:02 +0900

    api 연동 및 모델링 ui 구현

--- lib/app/api/product_provider.dart
+++ lib/app/api/product_provider.dart
@@ -5,12 +5,12 @@ import '../model/product/coffee/coffee_data_model.dart';
 //데이터 프로바이더.
 
 //사이클.
-
-//api 를 호출하는 프로바이더를 이곳에 선언 ->
+//api 를 호출하는 프로바이더를 선언 ->
 //각 UI 의 컨트롤러에서 프로바이더의 원하는 프로바이더를 호출 ->
 //프로바이더는 productFromJson 으로 데이터 모델에 데이터를 넘겨
 //flutter UI 에서 활용할수 있도록 바꿔줌 ->
-//최종 유저가 사용할 UI 에서 빌더를 사용해 UI 모델에 데이터 값을 하나씩 담아줌. 완성.
+//최종 유저가 사용할 UI 에서 빌더를 사용해 UI 모델에 데이터 값을 하나씩 담아줌. ->
+//유저가 시인할 UI에 빌드.
 class ProductProvider extends GetConnect implements GetxService {
   //api get 호출 메소드.
   Future<List<CoffeeProductModel>?> getStbProductData() async {
