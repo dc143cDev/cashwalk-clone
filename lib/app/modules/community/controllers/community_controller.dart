23214e5c2b3f1ae1c2e245e8f8f588252f549a85
dc143cDev <devc143c@gmail.com>
2023-01-30 23:54:33 +0900

    커뮤니티탭 UI

--- lib/app/modules/community/controllers/community_controller.dart
+++ lib/app/modules/community/controllers/community_controller.dart
@@ -67,15 +67,112 @@ class CommunityController extends GetxController {
             width: double.infinity,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
+              //상단 바.
+              //취소, 작성완료 텍스트버튼.
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('data'),
+                  TextButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    child: Text(
+                      '취소',
+                      style: TextStyle(
+                        color: textDark,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                  ),
+                  Text('글쓰기'),
+                  SizedBox(
+                    width: 100,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      //작성
+                      Get.back();
+                    },
+                    child: Text(
+                      '작성완료',
+                      style: TextStyle(
+                        color: textDark,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
+          Container(
+            width: double.infinity,
+            height: 70,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  SizedBox(
+                    width: 45,
+                    height: 45,
+                    child: CircleAvatar(),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  SizedBox(
+                    height: 40,
+                    width: 280,
+                    child: TextFormField(
+                      controller: titleController,
+                      autofocus: true,
+                      decoration: InputDecoration.collapsed(
+                        hintText: '제목(선택사항)',
+                        hintStyle: TextStyle(
+                          color: Colors.grey.shade300,
+                        ),
+                      ),
+                      maxLength: 16,
+                      maxLines: 1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.grey.shade200,
+            width: double.infinity,
+            height: 2,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(13.0),
+            child: Container(
+              width: 340,
+              height: 300,
+              child: TextFormField(
+                maxLines: 8,
+                maxLength: 100,
+                decoration: InputDecoration.collapsed(
+                  hintText: '내용',
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade300,
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       ),
       backgroundColor: bgColor,
